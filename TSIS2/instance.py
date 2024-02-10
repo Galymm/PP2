d503b54249b8d62dcb377808e7608a727754460d
Galymm <157203829+Galymm@users.noreply.github.com>
2024-02-10 15:17:54 +0600

    Add files via upload

---
+++ TSIS2/instance.py
@@ -0,0 +1,4 @@
+x = 200
+print(isinstance(x, int))
+
+#will check is 200 rlly integer
---
+++ TSIS2/onlyFalse.py
@@ -0,0 +1,10 @@
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+#In fact, there are not many values that evaluate to False,
+# except empty values, such as (), [], {}, "", the number 0, and the value None
